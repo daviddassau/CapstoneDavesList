412c957b5faf0f649b67801e40fe66cd83df2b1c
David Dassau <todieahero@hotmail.com>
2018-06-28 11:16:46 -0500

    wrote dto

--- CapstoneDavesList/Models/CategoryDto.cs
+++ CapstoneDavesList/Models/CategoryDto.cs
@@ -7,5 +7,10 @@ namespace CapstoneDavesList.Models
 {
     public class CategoryDto
     {
+        public int Id { get; set; }
+        public string CategoryName { get; set; }
+        public int CategoryId { get; set; }
+        public int RetailerId { get; set; }
+        public string Name { get; set; }
     }
 }
