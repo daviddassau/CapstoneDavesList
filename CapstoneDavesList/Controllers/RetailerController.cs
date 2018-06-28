97a846b3df949eb20028a573dc230795b4ee599f
David Dassau <todieahero@hotmail.com>
2018-06-28 10:34:59 -0500

    added RetailerDto, RetailerRepository, and wrote query to get all retailers

--- CapstoneDavesList/Controllers/RetailerController.cs
+++ CapstoneDavesList/Controllers/RetailerController.cs
@@ -4,10 +4,18 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using CapstoneDavesList.Services;
 
 namespace CapstoneDavesList.Controllers
 {
+    [RoutePrefix("api/retailer")]
     public class RetailerController : ApiController
     {
+        [HttpGet, Route("")]
+        public HttpResponseMessage GetRetailers()
+        {
+            var retailerRepository = new RetailerRepository();
+            var allRetailers = retailerRepository.GetAllRetailers();
+        }
     }
 }
---
+++ CapstoneDavesList/Services/RetailerRepository.cs
@@ -0,0 +1,34 @@
+﻿using CapstoneDavesList.Models;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using Dapper;
+
+namespace CapstoneDavesList.Services
+{
+    public class RetailerRepository
+    {
+        public SqlConnection CreateConnection()
+        {
+            return new SqlConnection(ConfigurationManager.ConnectionStrings["DavesList"].ConnectionString);
+        }
+
+        public IEnumerable<RetailerDto> GetAllRetailers()
+        {
+            using (var db = CreateConnection())
+            {
+                db.Open();
+
+                var allRetailers = db.Query<RetailerDto>(@"SELECT r.Id, r.Name, r.Location, r.Website, r.Details, rc.RetailerId, rc.CategoryId, c.CategoryName
+                                                           FROM Retailer r
+                                                           JOIN RetailerCategory rc on rc.RetailerId = r.Id
+                                                           JOIN Category c on c.Id = rc.CategoryId");
+
+                return allRetailers;
+            }
+        }
+    }
+}
