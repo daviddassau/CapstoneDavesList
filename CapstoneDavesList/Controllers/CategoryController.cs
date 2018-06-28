6372e3c9e1712c1d76de30e3bd9b269a971f746d
David Dassau <todieahero@hotmail.com>
2018-06-28 11:15:18 -0500

    writing query

--- CapstoneDavesList/Controllers/CategoryController.cs
+++ CapstoneDavesList/Controllers/CategoryController.cs
@@ -4,10 +4,18 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using CapstoneDavesList.Services;
 
 namespace CapstoneDavesList.Controllers
 {
+    [RoutePrefix("api/category")]
     public class CategoryController : ApiController
     {
+        [HttpGet, Route("")]
+        public HttpResponseMessage GetCategories()
+        {
+            var categoryRepository = new CategoryRepository();
+            var allCategories = categoryRepository.GetAllCategories();
+        }
     }
 }
--- CapstoneDavesList/Services/CategoryRepository.cs
+++ CapstoneDavesList/Services/CategoryRepository.cs
@@ -1,11 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
+using Dapper;
+using CapstoneDavesList.Models;
 
 namespace CapstoneDavesList.Services
 {
     public class CategoryRepository
     {
+        public SqlConnection CreateConnection()
+        {
+            return new SqlConnection(ConfigurationManager.ConnectionStrings["DavesList"].ConnectionString);
+        }
+
+        public IEnumerable<CategoryDto> GetAllCategories()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
