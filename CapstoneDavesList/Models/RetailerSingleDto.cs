8c49796992a535f83e22e02cae11c12d638b1b93
David Dassau <todieahero@hotmail.com>
2018-06-28 14:17:39 -0500

    working on Dto

--- CapstoneDavesList/Models/RetailerSingleDto.cs
+++ CapstoneDavesList/Models/RetailerSingleDto.cs
@@ -7,5 +7,13 @@ namespace CapstoneDavesList.Models
 {
     public class RetailerSingleDto
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Location { get; set; }
+        public string Website { get; set; }
+        public string Details { get; set; }
+        public int RetailerId { get; set; }
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; }
     }
 }
